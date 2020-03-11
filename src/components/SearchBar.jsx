15d7335e8f8c0375dfdec720f0ba93c2027efa40
issam1994 <issamaitouahmane@gmail.com>
2020-03-11 19:06:49 +0100

    updated styling for spinner & searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import {mdiMagnify as searchIcon} from '@mdi/js'
 
 export default function() {
     return (
-        <div className="flex items-center justify-end py-8">
+        <div className="flex items-center justify-end py-6">
             <div className="flex bg-white rounded-full overflow-hidden p-2 shadow">
                 {/* search icon  */}
                 <svg className="fill-current w-8 h-8 text-gray-300 mr-1" viewBox="0 0 22 22">
