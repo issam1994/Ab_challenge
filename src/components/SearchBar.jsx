58b7f8fd013ff3a0fbd35381b8bdcd893eafc32f
issam1994 <issamaitouahmane@gmail.com>
2020-03-11 15:53:23 +0100

    I guess I am done with search bar... in terms of markup and styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {mdiMagnify as searchIcon} from '@mdi/js'
 
 export default function() {
     return (
-        <div className="">
-            <div className="">
-                <input type="text" name="search" id="search"/>
+        <div className="flex items-center justify-end">
+            <div className="flex bg-white rounded-full overflow-hidden py-1 px-2 shadow">
+                <svg className="fill-current w-8 h-8 text-gray-300 mr-1" viewBox="0 0 22 22">
+                    <path d={searchIcon} />
+                </svg>
+                <input type="text" name="search" id="search" placeholder="Recherche par mot cle"/>
             </div>
         </div>
     )
